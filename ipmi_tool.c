c9abf14f6e06c2a7caee68d1879e462cc8a4e21b
sOmEoNe2lOvE <raymcraft@gmail.com>
2022-09-14 13:33:43 +0200

    Update

--- ipmi_tool.c
+++ ipmi_tool.c
@@ -133,6 +133,7 @@ linked_list_t *gather_sel(job_id_info_t *job_info)
 
     if ((log_fd = popen("ipmitool -U admin -P password sel list", "r")) == NULL)
         return (NULL);
+    printf("Adding SELs to list");
     sel_list = add_to_list(sel_list, init_parsed_sel);
     printf("Added to sel_list");
     while (getline(&buffer, &len, log_fd) != -1) {
