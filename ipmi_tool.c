bc9e2a65df6178e86ea2ac451e86121e863791cd
sOmEoNe2lOvE <raymcraft@gmail.com>
2022-09-14 10:52:42 +0200

    update

--- ipmi_tool.c
+++ ipmi_tool.c
@@ -137,6 +137,7 @@ int handle_sel_msg(parsed_sel_t *curr_sel)
     len --;
     curr_sel->sel_msg = strndup(&curr_sel->unparsed_sel[i - len], len);
     curr_sel->sel_msg[len] = '\0';
+    printf(curr_sel->sel_msg);
     return (0);
 }
 
