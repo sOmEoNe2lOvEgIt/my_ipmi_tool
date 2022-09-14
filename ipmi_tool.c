95a7980ed4688335ee1c178a01b9301ad6d1497b
sOmEoNe2lOvE <raymcraft@gmail.com>
2022-09-14 11:07:28 +0200

    Update

--- ipmi_tool.c
+++ ipmi_tool.c
@@ -78,35 +78,29 @@ int handle_sel_time(parsed_sel_t *curr_sel, time_t start_time)
     struct tm *sel_time = malloc(sizeof(struct tm));
     char time_str[80];
 
-    for (; curr_sel->unparsed_sel[i] != '|' && curr_sel->unparsed_sel[i] != '\0'; i++);
-    if (curr_sel->unparsed_sel[i] == '\0' && curr_sel->unparsed_sel[i + 2] == '\0')
+    i += len_untill(&curr_sel->unparsed_sel[i], '|') + 2;
+    if (curr_sel->unparsed_sel[i] == '\0')
         return (1);
-    i += 2;
     sel_time->tm_mday = atoi(&curr_sel->unparsed_sel[i]);
-    for (; curr_sel->unparsed_sel[i] != '/' && curr_sel->unparsed_sel[i] != '\0'; i++);
-    if (curr_sel->unparsed_sel[i] == '\0' && curr_sel->unparsed_sel[i + 1] == '\0')
+    i += len_untill(&curr_sel->unparsed_sel[i], '/') + 1;
+    if (curr_sel->unparsed_sel[i] == '\0')
         return (1);
-    i++;
     sel_time->tm_mon = atoi(&curr_sel->unparsed_sel[i]);
-    for (; curr_sel->unparsed_sel[i] != '/' && curr_sel->unparsed_sel[i] != '\0'; i++);
-    if (curr_sel->unparsed_sel[i] == '\0' && curr_sel->unparsed_sel[i + 1] == '\0')
+    i += len_untill(&curr_sel->unparsed_sel[i], '/') + 1;
+    if (curr_sel->unparsed_sel[i] == '\0')
         return (1);
-    i++;
     sel_time->tm_year = atoi(&curr_sel->unparsed_sel[i]);
-    for (; curr_sel->unparsed_sel[i] != '|' && curr_sel->unparsed_sel[i] != '\0'; i++);
-    if (curr_sel->unparsed_sel[i] == '\0' && curr_sel->unparsed_sel[i + 2] == '\0')
+    i += len_untill(&curr_sel->unparsed_sel[i], '|') + 2;
+    if (curr_sel->unparsed_sel[i] == '\0')
         return (1);
-    i += 2;
     sel_time->tm_hour = atoi(&curr_sel->unparsed_sel[i]);
-    for (; curr_sel->unparsed_sel[i] != ':' && curr_sel->unparsed_sel[i] != '\0'; i++);
-    if (curr_sel->unparsed_sel[i] == '\0' && curr_sel->unparsed_sel[i + 1] == '\0')
+    i += len_untill(&curr_sel->unparsed_sel[i], ':') + 1;
+    if (curr_sel->unparsed_sel[i] == '\0')
         return (1);
-    i++;
     sel_time->tm_min = atoi(&curr_sel->unparsed_sel[i]);
-    for (; curr_sel->unparsed_sel[i] != ':' && curr_sel->unparsed_sel[i] != '\0'; i++);
-    if (curr_sel->unparsed_sel[i] == '\0' && curr_sel->unparsed_sel[i + 1] == '\0')
+    i += len_untill(&curr_sel->unparsed_sel[i], ':') + 1;
+    if (curr_sel->unparsed_sel[i] == '\0')
         return (1);
-    i++;
     sel_time->tm_sec = atoi(&curr_sel->unparsed_sel[i]);
     sprintf(time_str, "[%d-%02d-%02dT%02d:%02d:%02d]", sel_time->tm_year, sel_time->tm_mon, sel_time->tm_mday, sel_time->tm_hour, sel_time->tm_min, sel_time->tm_sec);
     curr_sel->sel_time_str = strdup(time_str);
@@ -139,7 +133,6 @@ int handle_sel_msg(parsed_sel_t *curr_sel)
 
     for (; j < 3; j++, i++)
         i += len_untill(&curr_sel->unparsed_sel[i], '|');
-    printf(&curr_sel->unparsed_sel[i]);
     if (curr_sel->unparsed_sel[i] == '\0')
         return (1);
     i++;
