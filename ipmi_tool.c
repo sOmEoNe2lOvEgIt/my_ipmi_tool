708fdf91c8a43a7b7ee95ebc2c24436d399cf175
sOmEoNe2lOvE <raymcraft@gmail.com>
2022-09-14 11:11:11 +0200

    Update

--- ipmi_tool.c
+++ ipmi_tool.c
@@ -159,7 +159,7 @@ linked_list_t *gather_sel_logs
     while (getline(&buffer, &len, log_file) != -1) {
         curr_log = (parsed_sel_t *)sel_list->data;
         curr_log->unparsed_sel = strdup(buffer);
-        printf("%s\n\n", curr_log->unparsed_sel);
+        printf("%s", curr_log->unparsed_sel);
         if (handle_sel_time(curr_log, job_info->start_time))
             continue;
         else
@@ -175,7 +175,7 @@ linked_list_t *gather_sel_logs
         if (handle_sel_assert(curr_log))
             continue;
         else
-            printf("assert ok\n");
+            printf("assert ok\n\n");
         sel_list = add_to_list(sel_list, init_parsed_sel());
     }
     pclose(log_file);
